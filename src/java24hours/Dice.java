2de4143e5e74e340b26a0aa2d6773ad79cde6c8c
Tommy-Lim <tlim24@gmail.com>
2017-04-22 17:18:36 -0700

    added dice roll switch to int

--- src/java24hours/Dice.java
+++ src/java24hours/Dice.java
@@ -8,6 +8,9 @@ public class Dice {
     public static void main(String[] args){
         Random generator = new Random();
         int value = generator.nextInt();
+        double diceRoll = Math.floor(Math.random()*6+1);
+        int newDiceRoll = (int) diceRoll;
         System.out.println("The random number is: " + value);
+        System.out.println("The dice number is: " + newDiceRoll);
     }
 }
